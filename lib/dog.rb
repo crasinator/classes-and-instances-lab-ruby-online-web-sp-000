0f1c0cade9a807f49186e112078a51cf6a31da96
Jordan Reed <jordanreed03@gmail.com>
2020-12-23 23:16:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,13 @@
 class Dog 
   
-  fido = Dog.new
-  snoopy = Dog.new
-  lassie = Dog.new
+ def fido 
+   Dog.new
+ end 
+ 
+  def snoopy 
+    Dog.new
+  end 
+  def lassie
+    Dog.new
+  end 
 end 
